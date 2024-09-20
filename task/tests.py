e42116e1a8d9d287cd3b21cce25120bacbc46712
atrox3d <atrox3d@gmail.com>
2024-09-20 08:01:06 +0200

    add IndexPageTest.setUp

--- task/tests.py
+++ task/tests.py
@@ -17,6 +17,9 @@ class TaskModelTest(TestCase):
         self.assertEqual(str(task), task.title)
 
 class IndexPageTest(TestCase):
+    def setUp(self) -> None:
+        self.task = Task.objects.create(title='First Task')
+
     def test_index_page_returns_correct_response(self):
         response = self.client.get('/')
         print('test_index_page_returns_correct_response', f'{response = }')
@@ -25,10 +28,9 @@ class IndexPageTest(TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_index_page_has_tasks(self):
-        task = Task.objects.create(title='First Task')
 
         response = self.client.get('/')
         print('test_index_page_has_tasks', f'{response = }')
 
-        self.assertContains(response, task.title)
+        self.assertContains(response, self.task.title)
 
