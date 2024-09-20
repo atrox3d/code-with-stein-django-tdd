7e090c5edfe231dcf89c6193dfcf62e9d212ed54
atrox3d <atrox3d@gmail.com>
2024-09-20 06:06:00 +0200

    create test

--- task/tests.py
+++ task/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Task
+
+
+class TaskModelTest(TestCase):
+
+    def test_task_model_exist(self):
+        tasks = Task.objects.count()
+
+        self.assertEqual(tasks, 0)
+    
